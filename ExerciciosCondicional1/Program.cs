6e8f33e2d999de6412e2efee7be3d24c3e57b8c5
Evandro2270 <evandro535@gmail.com>
2024-12-05 11:29:21 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosCondicional1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int x = int.Parse(Console.ReadLine());
+
+                if (x < 0)
+                {
+                    Console.WriteLine("NEGATIVO: ");
+
+                }
+                else {
+                    Console.WriteLine("NÃO NEGATIVO");
+                }
+            }
+        }
+    }
+}
